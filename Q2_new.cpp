8408d67e72e68b6de31a7c619e81f2bd6de3aec2
Yashas Kamath <81322656+YashasKamath@users.noreply.github.com>
2022-10-26 23:43:40 +0530

    Update Q2_new.cpp

--- Q2_new.cpp
+++ Q2_new.cpp
@@ -1,90 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int no_of_digits(int n){
-    if(n==0)
-        return 1;
-    int count=0;
-    while(n>0){
-        count++;
-        n/=10;
-    }
-    return count;
-}
-
-void print_top_part(int n){
-    if(n==1){
-        cout<<"\xda\xc4\xc4\xc4\xc4\xc4\xbf\n";
-        return;
-    }
-    cout<<"\xda\xc4\xc4\xc4\xc4\xc4\xc2";
-    for(int i=0;i<n-2;i++)
-        cout<<"\xc4\xc4\xc4\xc4\xc4\xc2";
-    cout<<"\xc4\xc4\xc4\xc4\xc4\xbf\n";
-}
-
-void print_middle_part(vector<int>& v)
-{
-    cout<<"\xb3";
-    if(v.size()==1)
-    {
-        int digits=no_of_digits(v[0]);
-        if(digits==1)
-            cout<<"  "<<v[0]<<"  \xb3\n";
-        else if(digits==2)
-            cout<<" "<<v[0]<<"  \xb3\n";
-        else
-            cout<<" "<<v[0]<<" \xb3\n";
-        return;
-    }
-    
-    
-    for(int i=0;i<v.size();i++)
-    {
-        int digits=no_of_digits(v[i]);
-        if(digits==1)
-            cout<<"  "<<v[i]<<"  \xb3";
-        else if(digits==2)
-            cout<<" "<<v[i]<<"  \xb3";
-        else
-            cout<<" "<<v[i]<<" \xb3";
-    }
-    cout<<"\n";
-}
-
-void print_last_part(int n)
-{
-    if(n==1)
-    {
-        cout<<"\xc0\xc4\xc4\xc4\xc4\xc4\xd9"; 
-        return;
-    }
-    cout<<"\xc0\xc4\xc4\xc4\xc4\xc4"; 
-    for(int i=0;i<n-2;i++)
-        cout<<"\xc1\xc4\xc4\xc4\xc4\xc4"; 
-    cout<<"\xc1\xc4\xc4\xc4\xc4\xc4\xd9"; 
-}
-
-void print_using_horizontal_boxes(vector<int>& v)
-{
-    print_top_part(v.size());
-    print_middle_part(v);
-    print_last_part(v.size());
-}
-
-void print_pages(vector<int>& f, int max_cap){
-    if(!f.size()) return;
-    cout<<endl;
-    for(int i=0;i<max_cap*7;i++)
-        cout<<"\xcd";
-    cout<<endl;
-    print_using_horizontal_boxes(f);
-    cout<<endl;
-    for(int i=0;i<max_cap*3;i++)
-        cout<<"__";
-    cout<<endl;
-}
-
 class node{
     public: 
         int page;
@@ -242,38 +158,29 @@ int main(){
     cout<<"The sequence of pages stored in the frames of physical memory after each page is invoked is :\n";
     for(int i=0; i<n; i++){
         secondaryCache(primary, v[i], secondary);
-        cout<<"Primary LRU buffer contents: ";
-        vector<int> pri;
+        cout<<"\nPrimary LRU buffer contents: ";
         node* temp = primary.head->next;
         while(temp->page != -1){
-            pri.push_back(temp->page);
+            cout<<temp->page<<" ";
             temp = temp->next;
         }
-        print_pages(pri, capacity);
         cout<<"\nSecondary Ain FIFO buffer contents: ";
-        vector<int> secAin;
         temp = secondary.AinHead->next;
         while(temp->page != -1){
-            secAin.push_back(temp->page);
+            cout<<temp->page<<" ";
             temp = temp->next;
         }
-        print_pages(secAin, capacity >> 1);
-        vector<int> secAout;
         cout<<"\nSecondary Aout FIFO buffer contents: ";
         temp = secondary.AoutHead->next;
         while(temp->page != -1){
-            secAout.push_back(temp->page);
+            cout<<temp->page<<" ";
             temp = temp->next;
         }
-        print_pages(secAout, capacity >> 1);
         cout<<"\n";
-        for(int i=0;i<70;i++)
-            cout<<"\xdf\xdf";
-        cout<<endl;
     }
     return 0;
 }
 
 // 5
 // 20
-// 7 0 1 2 0 3 0 4 2 3 0 3 2 1 2 0 1 7 0 1
+// 7 0 1 2 0 3 0 4 2 3 0 3 2 1 2 0 1 7 0 1
