20aac894bf6ee3dbce4f2c3489a839d564cec446
EnergeticQuanta17 <115915946+EnergeticQuanta17@users.noreply.github.com>
2022-10-26 19:29:22 +0530

    LRU
    Least Recently Used

--- LRU_new.cpp
+++ LRU_new.cpp
@@ -74,27 +74,23 @@ void print_using_horizontal_boxes(vector<int>& v)
 
 void print_pages(vector<int>& f, int max_cap){
     if(!f.size()) return;
-    cout<<endl;
     for(int i=0;i<max_cap*7;i++)
         cout<<"\xcd";
     cout<<endl;
     print_using_horizontal_boxes(f);
-    cout<<endl;
-    for(int i=0;i<max_cap*3;i++)
-        cout<<"__";
-    cout<<endl;
+    
 }
 
 class node{
     public: 
-        int page;
-        node *next, *prev;
+        int val;
+        node *next,*prev;
         node(int v){
-            page=v;
+            val=v;
         }
 };
 
-class LRUPrimaryCache {
+class LRUCache {
 public:
     
     node *head=new node(-1);
@@ -102,138 +98,45 @@ public:
     int cap;
     map<int,node*> m;
     
-    LRUPrimaryCache(int capacity) {
+    LRUCache(int capacity) {
         cap=capacity;
         head->next=tail;
         tail->prev=head;
         head->prev=tail->next=NULL;
     }
     
-    void insert(int page){
-        node *temp=new node(page);
+    void insert(int val){
+        node *temp=new node(val);
         temp->next=head->next;
         temp->prev=head;
         head->next=temp;
         temp->next->prev=temp;
         cap--;
-        m[page]=temp;
+        m[val]=temp;
     }
     
     void Delete(node *t){
         t->prev->next=t->next;
         t->next->prev=t->prev;
         cap++;
-        m.erase(t->page);
+        m[t->val]=NULL;
     }
     
-    void put(int page) {
-        if(m[page] != NULL) Delete(m[page]);
-        else if(!cap) Delete(tail->prev);
-        insert(page);
-    }
-
-    bool isPresent(int page){
-        return m.find(page) != m.end();
+    void put(int value) {
+        if(m[value]!=NULL) Delete(m[value]);
+        else if(!cap){
+            cout<<"Page fault occurred... \n";
+            Delete(tail->prev);
+        }
+        insert(value);
     }
 };
-
-class LRUSecondaryCache{
-
-    public: 
-
-    map<int,node*> Ain, Aout;
-    node* AinHead = new node(-1);
-    node* AoutHead = new node(-1);
-    node* AinTail = new node(-1);
-    node* AoutTail = new node(-1);
-    int capAin, capAout;
-
-    LRUSecondaryCache(int capacity){
-        capAin = capAout = capacity;
-        AinHead->next = AinTail;
-        AinTail->prev = AinHead;
-        AinHead->prev = AinTail->next=NULL;
-        AoutHead->next = AoutTail;
-        AoutTail->prev = AoutHead;
-        AoutHead->prev = AoutTail->next=NULL;
-    }
-
-    bool isPresentAin(int page){
-        return Ain.find(page) != Ain.end();
-    }
-
-    bool isPresentAout(int page){
-        return Aout.find(page) != Aout.end();
-    }
-
-    void insertAin(int page){
-        if(Ain.find(page) != Ain.end()) return;
-        if(!capAin) DeleteAin();
-        node *temp=new node(page);
-        temp->next = AinHead->next;
-        AinHead->next = temp;
-        temp->prev = AinHead;
-        temp->next->prev = temp;
-        Ain[page] = temp;
-        capAin--;
-    }
-    
-    void DeleteAin(){
-        capAin++;
-        int last = AinTail->prev->page;
-        insertAout(last);
-        Ain.erase(last);
-        AinTail->prev->prev->next = AinTail;
-        AinTail->prev = AinTail->prev->prev;
-    }
-
-    void insertAout(int page){
-        if(Aout.find(page) != Aout.end()) return;
-        if(!capAout) DeleteAout(-1);
-        node *temp=new node(page);
-        temp->next = AoutHead->next;
-        temp->next->prev = temp;
-        AoutHead->next = temp;
-        temp->prev = AoutHead;
-        Aout[page] = temp;
-        capAout--;
-    }
-    
-    void DeleteAout(int page){
-        capAout++;
-        if(page != -1){
-            node* temp = Aout[page];
-            Aout.erase(page);
-            temp->prev->next = temp->next;
-            temp->next->prev = temp->prev;
-        }
-        else{
-            Aout.erase(AoutTail->prev->page);
-            AoutTail->prev->prev->next = AoutTail;
-            AoutTail->prev = AoutTail->prev->prev;
-        }
-    }
-};
-
-void secondaryCache(LRUPrimaryCache &primary, int page, LRUSecondaryCache &secondary){
-    if(primary.isPresent(page)) primary.put(page);
-    else if(secondary.isPresentAout(page)){
-        secondary.DeleteAout(page);
-        primary.put(page);
-    }
-    else if(secondary.isPresentAin(page)) return;
-    else{
-        cout<<"Page fault occurred...\n";
-        secondary.insertAin(page);
-    }
-}
 
 int main(){
     int capacity, n;
     cout<<"Enter the maximum number of frames possible: ";
     cin>>capacity;
-    LRUPrimaryCache primary(capacity);
-    LRUSecondaryCache secondary(capacity >> 1);
+    LRUCache lru(capacity);
     cout<<"Enter the number of pages invoked: ";
     cin>>n;
     vector<int> v(n);
@@ -241,39 +144,20 @@ int main(){
     for(int i=0; i<n; i++) cin>>v[i];
     cout<<"The sequence of pages stored in the frames of physical memory after each page is invoked is :\n";
     for(int i=0; i<n; i++){
-        secondaryCache(primary, v[i], secondary);
-        cout<<"Primary LRU buffer contents: ";
-        vector<int> pri;
-        node* temp = primary.head->next;
-        while(temp->page != -1){
-            pri.push_back(temp->page);
+        cout<<"After "<<(i+1)<<" pages invoked: \n";
+        lru.put(v[i]);
+        vector<int> t;
+        node* temp = lru.head->next;
+        while(temp->val != -1){
+            t.push_back(temp->val);
             temp = temp->next;
         }
-        print_pages(pri, capacity);
-        cout<<"\nSecondary Ain FIFO buffer contents: ";
-        vector<int> secAin;
-        temp = secondary.AinHead->next;
-        while(temp->page != -1){
-            secAin.push_back(temp->page);
-            temp = temp->next;
-        }
-        print_pages(secAin, capacity >> 1);
-        vector<int> secAout;
-        cout<<"\nSecondary Aout FIFO buffer contents: ";
-        temp = secondary.AoutHead->next;
-        while(temp->page != -1){
-            secAout.push_back(temp->page);
-            temp = temp->next;
-        }
-        print_pages(secAout, capacity >> 1);
+        print_pages(t, capacity);
         cout<<"\n";
+
         for(int i=0;i<70;i++)
             cout<<"\xdf\xdf";
         cout<<endl;
     }
     return 0;
 }
-
-// 5
-// 20
-// 7 0 1 2 0 3 0 4 2 3 0 3 2 1 2 0 1 7 0 1
